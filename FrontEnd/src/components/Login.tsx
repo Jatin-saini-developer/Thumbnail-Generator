f2802111e33a18f50f6bfb84d8747dac0de5060a
Jatin Saini <jatinrohitsaini@gmail.com>
2026-01-17 14:12:14 +0530

    Creating Login Page

--- FrontEnd/src/components/Login.tsx
+++ FrontEnd/src/components/Login.tsx
@@ -9,12 +9,12 @@ const Login = () => {
     password: "",
   });
 
-  const handleChange = (e) => {
+  const handleChange = (e: { target: { name: any; value: any; }; }) => {
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
     e.preventDefault();
   };
 
